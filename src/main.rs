93aafd08f7794c0a4545f18ae3b5305ea2f114e3
senet4er <senet4er@gmail.com>
2018-10-31 12:13:25 +0500

    Add basic GET routes

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
 extern crate warp;
 
+use warp::Filter;
+
 fn main() {
-    println!("Hello, world!");
+    const BIND_ADDRESS: ([u8; 4], u16) = ([0, 0, 0, 0], 8080);
+
+    let home = warp::path::end().map(|| "Home page");
+
+    let users = warp::path("users")
+        .and(warp::path::end())
+        .map(|| "Users page");
+
+    let players = warp::path("players")
+        .and(warp::path::end())
+        .map(|| "Players page");
+
+    let games = warp::path("games")
+        .and(warp::path::end())
+        .map(|| "Games page");
+
+    let routes = warp::get2().and(home.or(users).or(players).or(games));
+
+    warp::serve(routes).run(BIND_ADDRESS);
 }
